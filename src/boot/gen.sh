17f6f892060eb8129711a588fdafa706f1a326c5
root <root@localhost.localdomain>
2020-11-22 07:55:03 +0800

    [v0.5.2][upload]open page

--- src/boot/gen.sh
+++ src/boot/gen.sh
@@ -14,4 +14,4 @@ fi
 #fi
 
 nasm -I include/ -o $target_path/boot/mbr.bin mbr.S && dd if=$target_path/boot/mbr.bin of=$project_path/Origin.img bs=512 count=1  conv=notrunc
-nasm -I include/ -o $target_path/boot/loader.bin loader.S && dd if=$target_path/boot/loader.bin of=$project_path/Origin.img bs=512 count=1 seek=2 conv=notrunc
+nasm -I include/ -o $target_path/boot/loader.bin loader.S && dd if=$target_path/boot/loader.bin of=$project_path/Origin.img bs=512 count=4 seek=2 conv=notrunc
